b43dfb727997d2188672f4baa0ba1cb127e1d5a8
Rizal Nugroho <rizalnug2@gmail.com>
2023-09-17 07:52:02 +0000

    Starter model pegawai

---
+++ database/factories/PegawaiFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Crypt;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Pegawai>
+ */
+class PegawaiFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nama' => $this->faker->name(),
+            'nip' => Crypt::encryptString($this->faker->nik()),
+            'alamat' => Crypt::encryptString($this->faker->address()),
+        ];
+    }
+}
---
+++ database/seeders/PegawaiSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Pegawai;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PegawaiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Pegawai::factory(10)
+            ->create();
+    }
+}
