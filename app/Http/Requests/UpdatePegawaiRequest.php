9be8b839da4f957f70ee8f880250053f017ae874
Rizal Nugroho <rizalnug2@gmail.com>
2023-10-16 18:19:02 +0000

    update request mhs

--- app/Http/Requests/UpdatePegawaiRequest.php
+++ app/Http/Requests/UpdatePegawaiRequest.php
@@ -11,7 +11,7 @@ class UpdatePegawaiRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class UpdatePegawaiRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+        'nama' => 'sometimes|string|max:255',
+        'nip' => 'sometimes|string',
+        'alamat' => 'sometimes|string|nullable'
         ];
     }
 }
