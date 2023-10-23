b6306db34efa366519507240758a9018f9b6babc
Rizal Nugroho <Rizal>
2023-10-23 13:38:03 +0000

    Re-design tampilan login, register, route, package

---
+++ app/Http/Controllers/Auth/ProviderController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class ProviderController extends Controller
+{
+    public function redirect()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function callbackGoogle()
+    {
+        try {
+            $googleUser = Socialite::driver('google')->user();
+
+            $user = User::where('google_id', $googleUser->getId())->first();
+
+            if (!$user) {
+                $new_user = User::create([
+                    'name' => $googleUser->getName(),
+                    'email' => $googleUser->getEmail(),
+                    'google_id' => $googleUser->getId(),
+                ]);
+
+                Auth::login($new_user);
+
+                return redirect()->intended('dashboard');
+            } else {
+                Auth::login($user);
+
+                return redirect()->intended('dashboard');
+            }
+        } catch (\Throwable $th) {
+            dd('Something went wrong!' . $th->getMessage());
+        }
+    }
+}
