07ad33a64f586d6ccff87f3ad822cbe11f361463
Rizal Nugroho <Rizal>
2023-11-07 04:29:17 +0000

    authenticated user, role user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration {
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
+            $table->string('role')->default('user');
             $table->string('google_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
