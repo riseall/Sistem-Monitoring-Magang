e39ae1fb6c7415edd076a7ce248a7f90e4b5a965
= <=>
2023-12-04 14:32:22 +0700

    meringkas kode mejadi komponen

--- resources/js/Pages/Admin/Perusahaan.vue
+++ resources/js/Pages/Admin/Perusahaan.vue
@@ -2,24 +2,18 @@
 import Sidebar from '@/Components/Sidebar.vue';
 import { Head } from '@inertiajs/vue3';
 import axios from 'axios';
-import { Ref, computed, ref } from 'vue';
+import { Ref, ref } from 'vue';
 import BlueButton from '@/Components/UI/BlueButton.vue';
 import RedButton from '@/Components/UI/RedButton.vue';
 import ConfirmDialog from '@/Components/UI/ConfirmDialog.vue';
-
-import {
-    TransitionRoot,
-    TransitionChild,
-    Dialog,
-    DialogPanel,
-    DialogTitle,
-} from '@headlessui/vue'
+import ModalDialog from '@/Components/ModalDialog.vue';
+import InputForm from '@/Components/Form/InputForm.vue';
+import BtnTutup from '@/Components/UI/BtnTutup.vue';
 import BtnEdit from '@/Components/UI/BtnEdit.vue';
 import BtnDelete from '@/Components/UI/BtnDelete.vue';
 
 const isOpen = ref(false)
 const isEdit = ref(false)
-const selectedPerusahaan = ref<any | null>(null)
 
 const closeModal = () => (isOpen.value = false)
 const openModal = () => (isOpen.value = true)
@@ -89,6 +83,7 @@ const postData = async (event: Event) => {
     }
 }
 
+// Function to handle updating perusahaan data
 const updateData = async () => {
     const form = new FormData();
     form.append('nama', formData.value.nama);
@@ -131,6 +126,7 @@ const deleteUser = async (id: string) => {
 </script>
 <template>
     <head title="Perusahaan"></head>
+
     <Sidebar>
         <div class="container block bg-white border border-gray-100 shadow-md shadow-black/5 rounded-lg p-6 overflow-auto">
             <div class="flex justify-between mb-4 items-start-start">
@@ -145,6 +141,7 @@ const deleteUser = async (id: string) => {
                             <th class="th-items">No</th>
                             <th class="th-items">Nama Perusahaan</th>
                             <th class="th-items">Alamat Perusahaan</th>
+                            <th class="th-items">Action</th>
                         </tr>
                     </thead>
                     <tbody class="divide-y divide-gray-100">
@@ -165,134 +162,87 @@ const deleteUser = async (id: string) => {
             </div>
         </div>
 
-        <TransitionRoot appear :show="isOpen" as="template">
-            <Dialog as="div" @close="closeModal" class="relative z-10">
-                <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0" enter-to="opacity-100"
-                    leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0">
-                    <div class="fixed inset-0 bg-black bg-opacity-50" />
-                </TransitionChild>
-
-                <div class="fixed inset-0 overflow-y-auto">
-                    <div class="flex min-h-full items-center justify-center p-4 text-center">
-                        <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0 scale-95"
-                            enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100"
-                            leave-to="opacity-0 scale-95">
-                            <DialogPanel
-                                class="w-full lg:max-w-3xl md:max-w-lg sm:max-w-md transform overflow-hidden rounded-2xl bg-gray-100 p-6 text-left align-middle shadow-xl transition-all">
-                                <DialogTitle as="h3" class="text-lg font-medium leading-6 text-gray-900">
-                                    Tambah Data Perusahaan
-                                </DialogTitle>
-                                <div class="flex-auto px-4 lg:px-10 py-10 pb-0">
-                                    <form @submit.prevent="postData">
-                                        <div class="flex flex-wrap mt-3">
-                                            <div class="w-full lg:w-6/12 px-4">
-                                                <div class="relative w-full mb-3">
-                                                    <label
-                                                        class="block uppercase text-blueGray-600 text-xs font-bold mb-2 after:content-['*'] after:ml-0.5 after:text-red-500 required:border-red-500">
-                                                        Nama Perusahaan
-                                                    </label>
-                                                    <input type="text" v-model="formData.nama" placeholder="Nama"
-                                                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <hr class="mt-6 border-b-1 border-blueGray-300" />
-
-                                        <div class="flex flex-wrap mt-5">
-                                            <div class="w-full lg:w-12/12 px-4">
-                                                <div class="relative w-full mb-3">
-                                                    <label
-                                                        class="block uppercase text-blueGray-600 text-xs font-bold mb-2 after:content-['*'] after:ml-0.5 after:text-red-500">
-                                                        Alamat Perusahaan
-                                                    </label>
-                                                    <textarea type="text" v-model="formData.alamat" placeholder="Alamat"
-                                                        class="border-0 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150">
-                                                    </textarea>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="rounded-t mb-0 px-6 py-6">
-                                            <div class="text-center flex justify-end">
-                                                <BlueButton @click="postData, closeModal" type="submit" class="mr-2">Simpan
-                                                </BlueButton>
-                                                <RedButton @click="closeModal">Batal</RedButton>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </DialogPanel>
-                        </TransitionChild>
+        <!--dialog tambah data-->
+        <ModalDialog :is-open="isOpen" @close="closeModal">
+            <template v-slot:jdlDialog>Tambah Data Perusahaan</template>
+            <template v-slot:btn>
+                <BtnTutup @click="closeModal" />
+            </template>
+            <div class="flex-auto px-4 lg:px-10 py-10 pb-0">
+                <form @submit.prevent="postData">
+                    <div class="flex flex-wrap mt-3">
+                        <InputForm>
+                            <template v-slot:title>Nama</template>
+                            <template v-slot:input>
+                                <input type="text" v-model="formData.nama" placeholder="Nama" class="input-form" />
+                            </template>
+                        </InputForm>
                     </div>
-                </div>
-            </Dialog>
-        </TransitionRoot>
+
+                        <hr class="mt-6 border-b-1 border-blueGray-300" />
+
+                        <div class="flex flex-wrap mt-5">
+                        <InputForm>
+                            <template v-slot:title>Alamat</template>
+                            <template v-slot:input>
+                                <textarea type="text" v-model="formData.alamat" placeholder="Alamat"
+                                    class="input-form"></textarea>
+                            </template>
+                        </InputForm>
+                    </div>
+                    <div class="rounded-t mb-0 px-6 py-6">
+                        <div class="text-center flex justify-end">
+                            <BlueButton @click="postData, closeModal" type="submit" class="mr-2">Simpan</BlueButton>
+                            <RedButton @click="closeModal">Batal</RedButton>
+                        </div>
+                    </div>
+                </Form>
+            </div>    
+
+        </ModalDialog>
         <!-- End dialog tambah data -->
 
-                <!-- Dialog Edit Data -->
-        <TransitionRoot appear :show="isEdit" as="template">
-            <Dialog as="div" @close="editClose" class="relative z-10">
-                <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0" enter-to="opacity-100"
-                    leave="duration-200 ease-in" leave-from="opacity-100" leave-to="opacity-0">
-                    <div class="fixed inset-0 bg-black bg-opacity-50" />
-                </TransitionChild>
-
-                <div class="fixed inset-0 overflow-y-auto">
-                    <div class="flex min-h-full items-center justify-center p-4 text-center">
-                        <TransitionChild as="template" enter="duration-300 ease-out" enter-from="opacity-0 scale-95"
-                            enter-to="opacity-100 scale-100" leave="duration-200 ease-in" leave-from="opacity-100 scale-100"
-                            leave-to="opacity-0 scale-95">
-                            <DialogPanel
-                                class="w-full lg:max-w-3xl md:max-w-lg sm:max-w-md transform overflow-hidden rounded-2xl bg-gray-100 p-6 text-left align-middle shadow-xl transition-all">
-                                <DialogTitle as="h3" class="text-lg font-medium leading-6 text-gray-900">
-                                    Edit Data Perusahaan
-                                </DialogTitle>
-                                <div class="flex-auto px-4 lg:px-10 py-10 pb-0">
-                                    <form>
-                                        <div class="flex flex-wrap mt-3">
-                                            <div class="w-full lg:w-6/12 px-4">
-                                                <div class="relative w-full mb-3">
-                                                    <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2">
-                                                        Nama Perusahaan
-                                                    </label>
-                                                    <input type="text" id="nama" v-model="formData.nama"
-                                                        class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" />
-                                                </div>
-                                            </div>
-                                        </div>
-
-                                        <hr class="mt-6 border-b-1 border-blueGray-300" />
-
-                                        <div class="flex flex-wrap mt-5">
-                                            <div class="w-full lg:w-12/12 px-4">
-                                                <div class="relative w-full mb-3">
-                                                    <label class="block uppercase text-blueGray-600 text-xs font-bold mb-2">
-                                                        Alamat Perusahaan
-                                                    </label>
-                                                    <textarea type="text" v-model="formData.alamat"
-                                                        class="border-0 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150">
-                                                    </textarea>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="rounded-t mb-0 px-6 py-6">
-                                            <div class="text-center flex justify-end">
-                                                <BlueButton @click="updateData" class="mr-2">Simpan</BlueButton>
-                                                <RedButton @click="editClose">Batal</RedButton>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </DialogPanel>
-                        </TransitionChild>
+        <!-- Dialog Edit Data -->
+        <ModalDialog :is-open="isEdit">
+            <template v-slot:jdlDialog>Edit Data Perusahaan</template>
+            <template v-slot:btn>
+                <BtnTutup @click="editClose" />
+            </template>
+            <div class="flex-auto px-4 lg:px-10 py-10 pb-0">
+                <form @submit.prevent="updateData">
+                    <div class="flex flex-wrap mt-3">
+                        <InputForm>
+                            <template v-slot:title>Nama</template>
+                            <template v-slot:input>
+                                <input type="text" v-model="formData.nama" placeholder="Nama" class="input-form" />
+                            </template>
+                        </InputForm>
                     </div>
-                </div>
-            </Dialog>
-        </TransitionRoot>
+
+                    <hr class="mt-6 border-b-1 border-blueGray-300" />
+
+                    <div class="flex flex-wrap mt-5">
+                        <InputForm>
+                            <template v-slot:title>Alamat</template>
+                            <template v-slot:input>
+                                <textarea type="text" v-model="formData.alamat" placeholder="Alamat"
+                                    class="input-form"></textarea>
+                            </template>
+                        </InputForm>
+                    </div>
+                    <div class="rounded-t mb-0 px-6 py-6">
+                        <div class="text-center flex justify-end">
+                            <BlueButton @click="updateData" class="mr-2">Simpan</BlueButton>
+                            <RedButton @click="editClose">Batal</RedButton>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </ModalDialog>
         <!-- End Dialog Edit Data -->
 
         <ConfirmDialog v-if="isConfirmDialog">
-        <template v-slot:title>Konfirmasi</template>
+            <template v-slot:title>Konfirmasi</template>
             <template v-slot:message>Apakah anda yakin akan menghapus data?</template>
             <template v-slot:button>
                 <RedButton class="mx-1 mr-2">Ya</RedButton>
@@ -301,12 +251,3 @@ const deleteUser = async (id: string) => {
         </ConfirmDialog>
     </Sidebar>
 </template>
-<style>
-.th-items {
-    @apply p-2 text-[13px] font-medium border
-}
-
-.td-items {
-    @apply p-3 text-gray-600 text-sm font-normal ml-2 truncate
-}
-</style>
